19eb8d608640b32ad0073b0f6a4aa7140d74afa0
zetnitsun <108607064+zetnitsun@users.noreply.github.com>
2023-05-08 16:15:16 +0700

    Update Dog.py

--- Dog.py
+++ Dog.py
@@ -17,6 +17,7 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.utils import img_to_array, load_img
 import shutil
 from shutil import copyfile
+import cv
 
 st.set_page_config(
      page_title="Dog Breed Classifier",
